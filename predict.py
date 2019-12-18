438f84adf49c8631a7cece504a6b6846b483f03a
the0xA9 <yasinsky.a9@gmail.com>
2019-12-18 23:13:37 +0200

    initial commit

---
+++ predict.py
@@ -0,0 +1,34 @@
+# import the necessary packages
+from tensorflow.keras.models import load_model
+from skimage import transform
+from skimage import exposure
+from skimage import io
+import numpy as np
+
+def makePrediction():
+    # load the traffic sign recognizer model
+    # print("[INFO] loading model...")
+    model = load_model("trafficsignnet.model")
+
+    # load the label names
+    dictionary = open("signnames.csv").read().strip().split("\n")[1:]
+    # labelNames = [l.split(",")[1] for l in labelNames]
+
+    # load the image, resize it to 32x32 pixels, and then apply
+    # Contrast Limited Adaptive Histogram Equalization (CLAHE),
+    # just like we did during training
+    image = io.imread("image/image.jpg")
+    image = transform.resize(image, (32, 32))
+    image = exposure.equalize_adapthist(image, clip_limit=0.1)
+
+    # preprocess the image by scaling it to the range [0, 1]
+    image = image.astype("float32") / 255.0
+    image = np.expand_dims(image, axis=0)
+
+    # make predictions using the traffic sign recognizer CNN
+    prediction = model.predict(image)
+    index = prediction.argmax(axis=1)[0]
+
+    detectedSign = dictionary[index]
+    # print(detectedSign)
+    return (detectedSign)
---
+++ telegramBot.py
@@ -0,0 +1,46 @@
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+from predict import makePrediction
+
+BOT_TOKEN = "1017748021:AAEwedzebtXBG7HqrOLESyxnSyB5Wp7CaB0"
+
+class TelegramBot:
+    def __init__(self):
+        self.updater = Updater(token=BOT_TOKEN)
+
+        self.bot_url = 'https://t.me/trafficSignRecognitionBot'
+        self.updater.dispatcher.add_handler(CommandHandler('start', self.new_chat_user))
+        self.updater.dispatcher.add_handler(CommandHandler('help', self.help_message))
+        self.updater.dispatcher.add_handler(MessageHandler(Filters.photo, self.photo_handler))
+
+    @staticmethod
+    def get_bot_commands():
+        return {'/start': 'Begin your chat', '/help': 'List of bot commands'}
+
+    def start_bot(self):
+        self.updater.start_polling()
+
+    def help_message(self, bot, update):
+        chat_id = update.message.chat_id
+        command = self.get_bot_commands()
+        message = ''
+        for key in command.keys():
+            message += "{0} - {1}\n".format(key, command[key])
+        bot.sendMessage(chat_id=chat_id, text=message)
+
+    def new_chat_user(self, bot, update):
+        chat_id = update.message.chat_id
+        message = update.message.text.split('/start ', 1)
+        bot.sendMessage(chat_id=chat_id, text="Please, try again later")
+
+    def photo_handler(self, bot, update):
+        file = bot.getFile(update.message.photo[-1].file_id)
+        print ("file_id: " + str(update.message.photo[-1].file_id))
+        file.download('image/image.jpg')
+        detectedSign = makePrediction().split(",")
+        chat_id = update.message.chat_id
+        retMsg = "Sign \"" + detectedSign[1] + "\" detected\nSignID: " + detectedSign[2]
+        bot.sendMessage(chat_id=chat_id, text=retMsg)
+
+if __name__ == '__main__':
+    bot = TelegramBot()
+    bot.start_bot()
